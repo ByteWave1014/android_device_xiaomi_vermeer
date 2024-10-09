674e214d6815e8aa883b23bd269836a9f6fd0e47
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-10-08 20:09:59 -0500

    Revert "vermeer: We will use a kernel prebuilt"
    This reverts commit e37e2b85cc639c0751f528f8c7558bd18c66ff19.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,6 @@
 #
 
 DEVICE_PATH := device/xiaomi/vermeer
-KERNEL_PATH := device/xiaomi/vermeer-kernel
 
 # Inherit from sm8550-common
 include device/xiaomi/sm8550-common/BoardConfigCommon.mk
@@ -13,17 +12,6 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 530
 TARGET_USE_YCRCB_CAMERA_ENCODE := true
-
-# DTB
-BOARD_USES_DT := true
-BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtbs
-BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbs/dtbo.img
-
-TARGET_NO_KERNEL_OVERRIDE := true
-
-TARGET_KERNEL_SOURCE := $
-
-TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/Image
 
 # Kernel Modules
 device_second_stage_modules := \
